d507c9f2b48abd5224c25606e5be58380461fe8b
$krilla <damianblankenship@protonmail.com>
2023-10-04 09:43:58 -0400

    Added refresh sleep interval

--- refresh.py
+++ refresh.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 
 import sys
+import os
+import time
 from functions import *
 
-torrents = get_all('torrents')
+from dotenv import load_dotenv
+load_dotenv()
 
-if any(torrent['status'] == 'dead' for torrent in torrents):
-    for torrent in torrents:
-        if torrent['status'] == 'dead':
-            logging.warning('Found dead torrents')
-            refresh_torrent(torrent)
-else:
-    logging.info('Found no dead torrents')
+interval = int(os.getenv('REFRESH_INTERVAL', 15)) * 60 
 
-logging.info('Done')
+while True:
 
-sys.exit()
+    torrents = get_all('torrents')
+
+    if any(torrent['status'] == 'dead' for torrent in torrents):
+        for torrent in torrents:
+            if torrent['status'] == 'dead':
+                logging.warning('Found dead torrents')
+                refresh_torrent(torrent)
+    else:
+        logging.info('Found no dead torrents')
+
+    logging.info('Done')
+
+    logging.info('Sleeping %s minutes', int(interval / 60))
+
+    time.sleep(interval)
